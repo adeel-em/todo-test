c142ad04eeccfcf5077343324ef091e7e9fef30f
Muhammad Adeel <adeel@Muhammads-MacBook-Pro.local>
2024-05-21 01:04:08 +0500

    first commit

--- src/controllers/report-controller.js
+++ src/controllers/report-controller.js
@@ -1,202 +1,224 @@
-import { connect } from "http2"
-import redis from "redis"
+import { connect } from "http2";
+import redis from "redis";
 const client = redis.createClient({
-        port: process.env.REDIS_PORT,
-        host: process.env.REDIS_HOST,
-        password: process.env.REDIS_PASS,
-        no_ready_check: true
-    })
-const { promisify } = require('util')
-client.get = promisify(client.get)
-client.hget = promisify(client.hget)
-client.on("connect", function() {
-    console.error("redis connected");
+  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_HOST,
+  password: process.env.REDIS_PASS,
+  no_ready_check: true,
 });
-client.on("error", function(error) {
-    console.error(error);
+const { promisify } = require("util");
+client.get = promisify(client.get);
+client.hget = promisify(client.hget);
+client.on("connect", function () {
+  console.error("redis connected");
 });
-const { Task , Attachment, Sequelize:{ Op }, sequelize } = require('../../models')
+client.on("error", function (error) {
+  console.error(error);
+});
+const {
+  Task,
+  Attachment,
+  Sequelize: { Op },
+  sequelize,
+} = require("../../models");
 
-export async function totalTasks(req,res) {
+export async function totalTasks(req, res) {
+  var data = {
+    completed: 0,
+    incompleted: 0,
+    total: 0,
+    source: "mysql db",
+  };
 
-    var data = {
-        completed: 0,
-        incompleted: 0,
-        total: 0,
-        source: 'mysql db'
+  // check if cache exist
+  var cacheData = await client.get(req.user.id + `:report1`);
+
+  if (!cacheData) {
+    // Total tasks
+    let tasks = await Task.count({
+      where: {
+        userId: req.user.id,
+      },
+      group: ["status"],
+    }).catch((e) => console.log(e));
+
+    for (let val of tasks) {
+      data.total = Number(val.count) + Number(data.total);
+      if (val.status == 1) data.completed = val.count;
+      if (val.status == 0) data.incompleted = val.count;
     }
 
-    // check if cache exist 
-    var cacheData = await client.get(req.user.id + `:report1`)
-    
-    if(!cacheData)
-    {   
-        // Total tasks
-        let tasks = await Task.count({
-            where: {
-                userId: req.user.id,
-            },
-            group: ['status']
-        }).catch(e => console.log(e))
+    client.set(
+      req.user.id + `:report1`,
+      JSON.stringify(data),
+      "EX",
+      process.env.REDIS_EXPIRE
+    );
+  } else {
+    data = { ...JSON.parse(cacheData), source: "redis cache" };
+  }
 
- 
-        for(let val of tasks) {
-            data.total = Number(val.count) + Number(data.total)
-            if(val.status == 1) data.completed = val.count
-            if(val.status == 0) data.incompleted = val.count
-        }
-
-
-        client.set(req.user.id + `:report1`, JSON.stringify(data), 'EX', process.env.REDIS_EXPIRE)
-        
-    }else{
-        data = {  ...JSON.parse(cacheData), source: 'redis cache'}
-    }
-
-    return res.send({ data, status: 'success'})
-
+  return res.send({ data, status: "success" });
 }
-export async function averageCompletedTasksPerDay(req,res) {
+export async function averageCompletedTasksPerDay(req, res) {
+  var data = {
+    averageDailyTaskCompleted: "none",
+    source: "mysql db",
+  };
 
-    var data = {
-        averageDailyTaskCompleted: 'none',
-        source: 'mysql db'
+  // check if cache exist
+  var cacheData = await client.get(req.user.id + `:report2`);
+
+  if (!cacheData) {
+    // Total tasks
+    let dailyTaskCounts = await Task.findAll({
+      attributes: [[sequelize.fn("count", "id"), "dailytasks"]],
+      where: {
+        userId: req.user.id,
+        status: true,
+      },
+      group: [["completedAt"]],
+    }).catch((e) => console.log(e));
+
+    let total = 0;
+    let avg = 0;
+    if (dailyTaskCounts.length) {
+      for (let val of dailyTaskCounts) {
+        total = Number(val.dataValues.dailytasks) + Number(total);
+      }
+      avg = (total / dailyTaskCounts.length).toFixed(2);
     }
+    data.averageDailyTaskCompleted = avg;
+    client.set(
+      req.user.id + `:report2`,
+      JSON.stringify(data),
+      "EX",
+      process.env.REDIS_EXPIRE
+    );
+  } else {
+    data = { ...JSON.parse(cacheData), source: "redis cache" };
+  }
 
-    // check if cache exist 
-    var cacheData = await client.get(req.user.id + `:report2`)
-
-    if(!cacheData){
-
-        // Total tasks
-        let dailyTaskCounts = await Task.findAll({
-            attributes: [[sequelize.fn('count','id'),'dailytasks']],
-            where: {
-                userId: req.user.id,
-                status: true
-            },
-            group: [['completedAt']]
-        }).catch(e => console.log(e))
-
-        let total = 0
-        let avg = 0
-        if(dailyTaskCounts.length){
-
-            for(let val of dailyTaskCounts){
-                total = Number(val.dataValues.dailytasks) + Number(total)
-            }
-            avg = (total/dailyTaskCounts.length).toFixed(2)
-        }
-        data.averageDailyTaskCompleted = avg
-        client.set(req.user.id + `:report2`, JSON.stringify(data), 'EX', process.env.REDIS_EXPIRE)
-
-    }else{
-        data = {  ...JSON.parse(cacheData), source: 'redis cache'}
-    }
-
-    return res.send({ data, status: 'success'})
+  return res.send({ data, status: "success" });
 }
-export async function overDueTasks(req,res) {
+export async function overDueTasks(req, res) {
+  var data = {
+    overDueTasks: "none",
+    source: "mysql db",
+  };
 
-    var data = {
-        overDueTasks: 'none',
-        source: 'mysql db'
-    }
+  // check if cache exist
+  var cacheData = await client.get(req.user.id + `:report3`);
 
+  if (!cacheData) {
+    // Total tasks
+    let overDueTasks = await Task.count({
+      where: {
+        userId: req.user.id,
+        status: true,
+        completedAt: {
+          [Op.gt]: sequelize.col("dueDate"),
+        },
+      },
+    }).catch(console.log);
 
-    // check if cache exist 
-    var cacheData = await client.get(req.user.id + `:report3`)
+    data = { ...data, overDueTasks };
 
-    if(!cacheData)
-    {
-        // Total tasks
-        let overDueTasks = await Task.count({
-            where: {
-                userId: req.user.id,
-                status: true,
-                completedAt: {
-                    [Op.gt]: sequelize.col('dueDate')
-                }
-            }
-        }).catch(console.log)
+    client.set(
+      req.user.id + `:report3`,
+      JSON.stringify(data),
+      "EX",
+      process.env.REDIS_EXPIRE
+    );
+  } else {
+    data = { ...JSON.parse(cacheData), source: "redis cache" };
+  }
 
-        data = {  ...data, overDueTasks}
-
-        client.set(req.user.id + `:report3`, JSON.stringify(data), 'EX', process.env.REDIS_EXPIRE)
-
-    } else {
-        data = {  ...JSON.parse(cacheData), source: 'redis cache'}
-    }
-
-    return res.send({ data, status: 'success'})
+  return res.send({ data, status: "success" });
 }
-export async function maxTasksCompletionDay(req,res) {
+export async function maxTasksCompletionDay(req, res) {
+  var data = {
+    source: "mysql db",
+  };
 
+  // check if cache exist
+  var cacheData = await client.get(req.user.id + `:report4`);
+  if (!cacheData) {
+    let dailyTaskCounts = await Task.findAll({
+      attributes: [
+        [sequelize.fn("count", "id"), "taskCompleted"],
+        "completedAt",
+      ],
+      where: {
+        userId: req.user.id,
+        completedAt: {
+          [Op.ne]: null,
+        },
+      },
+      group: [["completedAt"]],
+    }).catch((e) => console.log(e));
 
-    var data = {
-        source: 'mysql db'
+    let maximumTasks = { taskCompleted: 0, completedAt: null };
+    for (let task of dailyTaskCounts) {
+      if (task.dataValues.taskCompleted > maximumTasks.taskCompleted) {
+        maximumTasks = {
+          taskCompleted: task.dataValues.taskCompleted,
+          completedAt: task.completedAt,
+        };
+      }
     }
 
+    let day = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    maximumTasks.dayName = day[new Date(maximumTasks.completedAt).getDay()];
 
-    // check if cache exist 
-    var cacheData = await client.get(req.user.id + `:report4`)
-    if(!cacheData)
-    {
-        let dailyTaskCounts = await Task.findAll({
-            attributes: [[sequelize.fn('count','id'),'taskCompleted'], 'completedAt'],
-            where: {
-                userId: req.user.id,
-                completedAt: {
-                    [Op.ne]: null
-                }
-            },
-            group: [['completedAt']]
-        }).catch(e => console.log(e))
+    data = { ...data, ...maximumTasks };
+    client.set(
+      req.user.id + `:report4`,
+      JSON.stringify(data),
+      "EX",
+      process.env.REDIS_EXPIRE
+    );
+  } else {
+    data = { ...JSON.parse(cacheData), source: "redis cache" };
+  }
 
-        let maximumTasks = {taskCompleted:0,completedAt:null}
-        for(let task of dailyTaskCounts) {
-            if(task.dataValues.taskCompleted > maximumTasks.taskCompleted){
-                maximumTasks = { 
-                    taskCompleted: task.dataValues.taskCompleted,
-                    completedAt: task.completedAt,
-                }
-            }
-        }
-
-        let day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-        maximumTasks.dayName = day[new Date(maximumTasks.completedAt).getDay()]
-
-        data = { ...data, ...maximumTasks }        
-        client.set(req.user.id + `:report4`, JSON.stringify(data), 'EX', process.env.REDIS_EXPIRE)
-
-    } else {
-        data = {  ...JSON.parse(cacheData), source: 'redis cache'}
-    }
-    
-    return res.send({ 
-        data, 
-        status: 'success'
-    })
+  return res.send({
+    data,
+    status: "success",
+  });
 }
-export async function tasksOpenInDayOfWeek(req,res) {
+export async function tasksOpenInDayOfWeek(req, res) {
+  var data = {
+    source: "mysql db",
+  };
 
-    var data = {
-        source: 'mysql db'
-    }
+  // check if cache exist
+  var cacheData = await client.get(req.user.id + `:report5`);
+  if (!cacheData) {
+    let sql =
+      "SELECT DAYNAME(`startDate`) as `day`, count(*) AS `tasksopened` FROM `Tasks` AS `Task` WHERE `Task`.`userId` = " +
+      req.user.id +
+      " GROUP BY DAYNAME(`startDate`)";
+    let allTasks = await sequelize.query(sql, null, { raw: true });
 
-    // check if cache exist 
-    var cacheData = await client.get(req.user.id + `:report5`)
-    if(!cacheData)
-    {
-        let sql = "SELECT DAYNAME(`startDate`) as `day`, count(*) AS `tasksopened` FROM `Tasks` AS `Task` WHERE `Task`.`userId` = " + req.user.id + " GROUP BY DAYNAME(`startDate`)"
-        let allTasks = await sequelize.query(sql, null, {raw: true})
+    data = { ...allTasks[0], ...data };
+    client.set(
+      req.user.id + `:report5`,
+      JSON.stringify(data),
+      "EX",
+      process.env.REDIS_EXPIRE
+    );
+  } else {
+    data = { ...JSON.parse(cacheData), source: "redis cache" };
+  }
 
-        data = { ...allTasks[0], ...data}        
-        client.set(req.user.id + `:report5`, JSON.stringify(data), 'EX', process.env.REDIS_EXPIRE)
-
-    } else {
-        data = {  ...JSON.parse(cacheData), source: 'redis cache'}
-    }
-    
-    return res.send({ data, status: 'success'})
+  return res.send({ data, status: "success" });
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,98 +1,86 @@
-import express from "express"
-import path from "path"
-const router = new express.Router()
-import multer from "multer"
-
+import express from "express";
+import path from "path";
+const router = new express.Router();
+import multer from "multer";
 
 // multer filer upload storage
 const storage = multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null,'uploads/')
-    },
-    filename: function(req,file,cb){
-        cb(null,Date.now() + path.extname(file.originalname))
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
 
 const upload = multer({
-    storage
-})
+  storage,
+});
 
 // middlewares
 
-import { auth, authUnverified } from "../middlewares/auth"
-import  filters from "../middlewares/filters"
-import * as user from "../controllers/user-controller"
-import * as taskController from "../controllers/task-controller"
+import { auth, authUnverified } from "../middlewares/auth";
+import filters from "../middlewares/filters";
+import * as user from "../controllers/user-controller";
+import * as taskController from "../controllers/task-controller";
 const {
-    totalTasks,
-    averageCompletedTasksPerDay,
-    overDueTasks,
-    maxTasksCompletionDay,
-    tasksOpenInDayOfWeek,
-} = require('../controllers/report-controller')
-
-
-
-
-
-
+  totalTasks,
+  averageCompletedTasksPerDay,
+  overDueTasks,
+  maxTasksCompletionDay,
+  tasksOpenInDayOfWeek,
+} = require("../controllers/report-controller");
 
 //User
-router.post('/v1/signin', user.signin)
-router.post('/v1/signup', user.signup)
-router.post('/v1/verify-email', authUnverified , user.verifyEmail)
-router.post('/v1/signout', [auth], user.signout)
-router.get('/v1/users', [filters], user.index)
-router.get('/v1/user/profile', [auth], user.profile)
-router.put('/v1/user/profile', [auth], user.update)
-router.delete('/v1/user/:id', [auth], user.destroy)
-
+router.post("/v1/signin", user.signin);
+router.post("/v1/signup", user.signup);
+router.post("/v1/verify-email", authUnverified, user.verifyEmail);
+router.post("/v1/signout", [auth], user.signout);
+router.get("/v1/users", [filters], user.index);
+router.get("/v1/user/profile", [auth], user.profile);
+router.put("/v1/user/profile", [auth], user.update);
+router.delete("/v1/user/:id", [auth], user.destroy);
 
 //Task
-router.get('/v1/tasks', [ auth, filters], taskController.index)
-router.post('/v1/task', [ auth ], taskController.create)
-router.get('/v1/task/:id', [ auth ], taskController.view)
-router.get('/v1/task/:id/edit', [ auth ], taskController.edit)
-router.put('/v1/task/:id', [auth], taskController.update)
-router.delete('/v1/task/:id', [auth], taskController.destroy)
-
+router.get("/v1/tasks", [auth, filters], taskController.index);
+router.post("/v1/task", [auth], taskController.create);
+router.get("/v1/task/:id", [auth], taskController.view);
+router.get("/v1/task/:id/edit", [auth], taskController.edit);
+router.put("/v1/task/:id", [auth], taskController.update);
+router.delete("/v1/task/:id", [auth], taskController.destroy);
 
 // Attachments
-router.post('/v1/upload/', [
-        auth, 
-        // (req,res,next) => { console.log(req.body.taskId); if(!req.body.taskId) return res.status(400).send({ status: "error", message: "taskId missing" }); else next() } ,
-        upload.single('file')
-    ],
-    taskController.attachments,
-    (err, req, res, next) => {
-    res.send({error: err.message})
-})
-
-
+router.post(
+  "/v1/upload/",
+  [
+    auth,
+    // (req,res,next) => { console.log(req.body.taskId); if(!req.body.taskId) return res.status(400).send({ status: "error", message: "taskId missing" }); else next() } ,
+    upload.single("file"),
+  ],
+  taskController.attachments,
+  (err, req, res, next) => {
+    res.send({ error: err.message });
+  }
+);
 
 // Reports
 
-router.get('/v1/report/totalTasks', [ auth ], totalTasks)
-router.get('/v1/report/averageCompletedTasksPerDay', [ auth ], averageCompletedTasksPerDay)
-router.get('/v1/report/overDueTasks', [ auth ], overDueTasks)
-router.get('/v1/report/maxTasksCompletionDay', [ auth ], maxTasksCompletionDay)
-router.get('/v1/report/tasksOpenInDayOfWeek', [ auth ], tasksOpenInDayOfWeek)
-
-
-
+router.get("/v1/report/totalTasks", [auth], totalTasks);
+router.get(
+  "/v1/report/averageCompletedTasksPerDay",
+  [auth],
+  averageCompletedTasksPerDay
+);
+router.get("/v1/report/overDueTasks", [auth], overDueTasks);
+router.get("/v1/report/maxTasksCompletionDay", [auth], maxTasksCompletionDay);
+router.get("/v1/report/tasksOpenInDayOfWeek", [auth], tasksOpenInDayOfWeek);
 
 // Similar tasks
-router.get('/v1/similar-tasks', auth, taskController.similarTasks)
-
-
+router.get("/v1/similar-tasks", auth, taskController.similarTasks);
 
 // Test email endpoints
 // router.get('/v1/test-reminder-email', scheduleController.sendReminderEmail)
 // router.get('/v1/test-sessions', taskController.sessions)
 // router.get('/v1/test-sessions-get', taskController.sessionsGet)
 
-
-export default router
-
-
+export default router;
